4e97f6c29f4564fd07bc6cd5e491b4d764fdfef0
BYUMVUHORE Aimable <aimablebyumvuhore@gmail.com>
2025-03-06 06:56:31 +0200

    Addition of star rating and  image slider file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
+import "./App.css";
 import Accordion from "./components/accordian";
 import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
       <Accordion />
       {/* Random color component */}
-      <RandomColor/> 
+      <RandomColor />
+      {/* Star rating component */}
+      <StarRating />
       
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
